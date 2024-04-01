db0e8bef1cf6645bb1e24ea82c05edd736e94082
AsankaThennakoon <newsunflowers98@gmail.com>
2024-04-01 14:09:41 +0530

    complet base funtionlities of lecture

--- lib/screen/lecture/edite_restult_with_filters.dart
+++ lib/screen/lecture/edite_restult_with_filters.dart
@@ -1,118 +1,266 @@
-import 'package:excel/excel.dart';
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:result_ease/models/result.dart';
+import 'package:result_ease/models/student.dart';
+import 'package:result_ease/models/results.dart';
+import 'package:result_ease/screen/lecture/edite_result.dart';
 import 'package:result_ease/widgets/custom_drop_down_item.dart';
-import 'package:result_ease/widgets/custom_file_upload.dart';
-import 'dart:io';
+import 'package:result_ease/utils/app_colors.dart';
+import 'package:result_ease/widgets/custom_back_button.dart';
+import 'package:result_ease/widgets/custom_button.dart';
 
-import '../../helpers/dialog_helper.dart';
-import '../../utils/app_colors.dart';
-import '../../widgets/custom_back_button.dart';
-import '../../widgets/custom_button.dart';
+import '../../widgets/custom_text_field.dart';
 
 class EditResultsWithFilters extends StatefulWidget {
-  const EditResultsWithFilters({super.key});
+  const EditResultsWithFilters({Key? key}) : super(key: key);
 
   @override
-  State<EditResultsWithFilters> createState() => _EditResultsWithFiltersState();
+  _EditResultsWithFiltersState createState() => _EditResultsWithFiltersState();
 }
 
 class _EditResultsWithFiltersState extends State<EditResultsWithFilters> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-
   final TextEditingController _year = TextEditingController();
   final TextEditingController _batch = TextEditingController();
   final TextEditingController _semester = TextEditingController();
+  final TextEditingController _name = TextEditingController();
+  final TextEditingController _indexNo = TextEditingController();
+  final TextEditingController _subject = TextEditingController();
+  final TextEditingController _grade = TextEditingController();
 
   List<String> _yearList = [];
   List<String> _batchList = [];
   List<String> _semesterList = [];
- 
-  final _firebaseAuth = FirebaseAuth.instance;
-
+  var _studentsList = [];
+  List<String> _nameList = [];
+  List<String> _indexList = [];
+  List<String> _subjectList = [];
+  String? _subjectGrade;
   bool _isLoading = false;
+  bool _isSubjectLoading = false;
+  Map<String, String>? courseData;
+  String? selctedCourseCode;
+  List<Result> _result = [];
 
   @override
   void initState() {
     super.initState();
-    // Load data from Firestore
     _loadData();
   }
 
-
   Future<void> _loadData() async {
-    final batchSnapshots = await FirebaseFirestore.instance
-        .collection('batches')
-        .where('userId', isEqualTo: _auth.currentUser?.uid) //
+    setState(() => _isLoading = true);
+    try {
+      final batchSnapshots = await FirebaseFirestore.instance
+          .collection('batches')
+          .where('userId', isEqualTo: _auth.currentUser?.uid)
+          .get();
+
+      final years = <String>{};
+      final batches = <String>{};
+      final semesters = <String>{};
+
+      batchSnapshots.docs.forEach((batch) {
+        final data = batch.data();
+        years.add(data['year']);
+        batches.add(data['batch']);
+        semesters.add(data['semester']);
+      });
+
+      setState(() {
+        _yearList = years.toList();
+        _batchList = batches.toList();
+        _semesterList = semesters.toList();
+      });
+    } catch (error) {
+      print('Error loading data: $error');
+    } finally {
+      setState(() => _isLoading = false);
+    }
+  }
+
+  Future<List<dynamic>> _fetchStudents(
+      String batch, String semester, String year) async {
+    final snapshot = await FirebaseFirestore.instance
+        .collection('results')
+        .where('batch', isEqualTo: batch)
+        .where('semester', isEqualTo: semester)
+        .where('year', isEqualTo: year)
         .get();
 
-    final years = <String>{};
-    final batches = <String>{};
-    final semesters = <String>{};
+    return snapshot.docs.map((doc) => Student.fromJson(doc.data())).toList();
+  }
 
-    for (final batch in batchSnapshots.docs) {
-      final data = batch.data() as Map<String, dynamic>;
-      years.add(data['year']);
-      batches.add(data['batch']);
-      semesters.add(data['semester']);
+  void _onSemesterSelected(String value) async {
+    setState(() {
+      _nameList.clear();
+      _indexList.clear();
+      _isLoading = true;
+    });
+
+    try {
+      _studentsList = await _fetchStudents(_batch.text, value, _year.text);
+      _studentsList.forEach((student) {
+        _nameList.add(student.name);
+        _indexList.add(student.indexNo);
+      });
+    } catch (error) {
+      print('Error loading students: $error');
+    } finally {
+      setState(() => _isLoading = false);
+    }
+  }
+
+  void _onSelectStudentName(String name) {
+    setState(() {
+      _indexList.clear();
+      _isSubjectLoading = true;
+    });
+
+    String? seletedStudentIndexNO;
+
+    for (var element in _studentsList) {
+      Student temp = element as Student;
+      if (temp.name == name) {
+        seletedStudentIndexNO = temp.indexNo;
+      }
     }
 
+    if (seletedStudentIndexNO != null) {
+      setState(() {
+        _indexList.add(seletedStudentIndexNO!);
+      });
+    }
+  }
+
+  void _onSelectIndexNo(String indexNo) {
     setState(() {
-      _yearList = years.toList();
-      _batchList = batches.toList();
-      _semesterList = semesters.toList();
+      _nameList.clear();
+      _isSubjectLoading = true;
     });
+
+    String? seletedStudentName;
+
+    for (var element in _studentsList) {
+      Student temp = element as Student;
+      if (temp.indexNo == indexNo) {
+        seletedStudentName = temp.name;
+      }
+    }
+
+    if (seletedStudentName != null) {
+      setState(() {
+        _nameList.add(seletedStudentName!);
+      });
+    }
+
+    _loadSubject(indexNo);
   }
 
-  void _save() async {
-   
-  }
-
-  void _cancle() async {
+  void _loadSubject(String indexNo) {
     setState(() {
-      _year.clear(); // Clear the text in the year controller
-      _batch.clear(); // Clear the text in the batch controller
-      _semester.clear(); // Clear the text in the semester controller
-     
+      _subject.clear();
+    });
+    try {
+      for (var element in _studentsList) {
+        Student temp = element as Student;
+        if (temp.indexNo == indexNo) {
+          courseData = temp.courseData;
+          _result = temp.courseData.entries
+              .map((entry) => Result(subject: entry.key, grade: entry.value))
+              .toList();
+
+          print("HIHIHIHIHIH" + _result.toString());
+
+          for (var element in _result) {
+            _subjectList.add(element.subject);
+            // _gradeList.add(element.grade);
+          }
+        }
+      }
+    } catch (e) {
+      print("this is comming form subject loading" + e.toString());
+    }
+  }
+
+  void _onSelectSubject(String subject) {
+    for (var element in _result) {
+      if (element.subject == subject) {
+        setState(() {
+          selctedCourseCode = subject;
+          _subjectGrade = element.grade;
+          _grade.text = _subjectGrade!;
+        });
+      }
+    }
+  }
+
+  void _edite() async {
+    Results object = Results(
+        name: _name.text,
+        indexNo: _indexNo.text,
+        courseData: courseData!,
+        batch: _batch.text,
+        semester: _semester.text,
+        year: _year.text,
+        userId: _auth.currentUser!.uid);
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => EditeResult(
+                  result: object,
+                  subjectCode: selctedCourseCode!,
+                )));
+  }
+
+  void _cancel() {
+    setState(() {
+      _year.clear();
+      _batch.clear();
+      _semester.clear();
+      _name.clear();
+      _indexNo.clear();
+      _subject.clear();
+      _grade.clear();
+      _nameList.clear();
+      _indexList.clear();
+      _subjectList.clear();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    var screenSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(55), //width and height
-          child: SafeArea(
-              child: Stack(
+        preferredSize: const Size.fromHeight(55),
+        child: SafeArea(
+          child: Stack(
             children: [
               CustomBackButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
+                onPressed: () => Navigator.pop(context),
               ),
             ],
-          ))),
+          ),
+        ),
+      ),
       backgroundColor: AppColors.backgroundColor,
       resizeToAvoidBottomInset: false,
       body: GestureDetector(
-        onTap: () {
-          FocusScope.of(context).requestFocus(FocusNode());
-        },
+        onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
         child: Container(
-          height: screenSize.height,
+          height: MediaQuery.of(context).size.height,
           decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(color: Colors.blueGrey[200] as Color, blurRadius: 10)
-              ],
-              color: Colors.white,
-              borderRadius: const BorderRadius.all(
-                Radius.circular(12),
-              )),
+            boxShadow: [
+              BoxShadow(color: Colors.blueGrey[200]!, blurRadius: 10),
+            ],
+            color: Colors.white,
+            borderRadius: const BorderRadius.all(
+              Radius.circular(12),
+            ),
+          ),
           margin:
               const EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 20),
           padding:
@@ -120,8 +268,8 @@ class _EditResultsWithFiltersState extends State<EditResultsWithFilters> {
           child: SingleChildScrollView(
             child: ConstrainedBox(
               constraints: BoxConstraints(
-                minWidth: screenSize.width,
-                minHeight: screenSize.height - 130,
+                minWidth: MediaQuery.of(context).size.width,
+                minHeight: MediaQuery.of(context).size.height - 130,
               ),
               child: IntrinsicHeight(
                 child: Stack(
@@ -131,8 +279,8 @@ class _EditResultsWithFiltersState extends State<EditResultsWithFilters> {
                       left: 0,
                       child: SvgPicture.asset(
                         'assets/images/top_circle.svg',
-                        width: 200, // Adjust the width as needed
-                        height: 200, // Adjust the height as needed
+                        width: 200,
+                        height: 200,
                       ),
                     ),
                     Form(
@@ -153,18 +301,43 @@ class _EditResultsWithFiltersState extends State<EditResultsWithFilters> {
                             controller: _semester,
                             labelName: "Selected Semester",
                             listValues: _semesterList,
+                            onChanged: _onSemesterSelected,
                           ),
                           CustomDropDownItem(
-                            controller: _semester,
+                            isEnabled: _nameList.isNotEmpty && !_isLoading,
+                            controller: _name,
+                            labelName: (_nameList.isEmpty)
+                                ? "Selected Student Name"
+                                : _nameList.length == 1
+                                    ? _nameList.first
+                                    : "Selected Student Name",
+                            listValues: _nameList,
+                            onChanged: _onSelectStudentName,
+                            initalValue:
+                                _nameList.length == 1 ? _nameList.first : null,
+                          ),
+                          CustomDropDownItem(
+                            isEnabled: _indexList.isNotEmpty && !_isLoading,
+                            controller: _indexNo,
                             labelName: "Selected Student IndexNo",
-                            listValues: _semesterList,
+                            listValues: _indexList,
+                            onChanged: _onSelectIndexNo,
+                            initalValue: _indexList.length == 1
+                                ? _indexList.first
+                                : null,
                           ),
                           CustomDropDownItem(
-                            controller: _semester,
+                            isEnabled: _subjectList.isNotEmpty,
+                            controller: _subject,
                             labelName: "Selected Subject",
-                            listValues: _semesterList,
+                            listValues: _subjectList,
+                            onChanged: _onSelectSubject,
                           ),
-                          
+                          const SizedBox(height: 10,),
+                          CustomTextField(
+                              isEditable: false,
+                              labelName: "Grade",
+                              controller: _grade),
                           const SizedBox(
                             height: 10,
                           ),
@@ -174,16 +347,18 @@ class _EditResultsWithFiltersState extends State<EditResultsWithFilters> {
                               _isLoading
                                   ? CircularProgressIndicator()
                                   : CustomButton(
-                                      onClick: _save,
-                                      label: "SAVE",
-                                      color: AppColors.buttonColorDark),
+                                      onClick: _edite,
+                                      label: "Edit",
+                                      color: AppColors.buttonColorDark,
+                                    ),
                               const SizedBox(
                                 width: 10,
                               ),
                               CustomButton(
-                                  onClick: _cancle,
-                                  label: "CANCEL",
-                                  color: AppColors.accentColor),
+                                onClick: _cancel,
+                                label: "CANCEL",
+                                color: AppColors.accentColor,
+                              ),
                             ],
                           ),
                           const SizedBox(
--- lib/widgets/custom_drop_down_item.dart
+++ lib/widgets/custom_drop_down_item.dart
@@ -1,29 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
 import '../utils/app_colors.dart';
 
 class CustomDropDownItem extends StatefulWidget {
-   final String labelName;
+  final String labelName;
   final TextEditingController controller;
   final List<String> listValues;
-  const CustomDropDownItem({super.key,required this.labelName,
+  final bool isEnabled;
+  final String ? initalValue;
+  final Function(String)? onChanged; // Add this field
+
+  const CustomDropDownItem({
+    Key? key,
+    required this.labelName,
     required this.controller,
-    required this.listValues});
+    required this.listValues,
+    this.isEnabled = true,
+    this.initalValue,
+    this.onChanged, // Default value for isEnabled
+  }) : super(key: key);
 
   @override
   State<CustomDropDownItem> createState() => _CustomDropDownItemState();
 }
 
 class _CustomDropDownItemState extends State<CustomDropDownItem> {
-  String ? selectedYear;
+  String? selectedYear;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    if(widget.initalValue!=null){
+      selectedYear=widget.initalValue;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 60,
-      margin: const EdgeInsets.only(left: 30, right: 30,top:14),
+      margin: const EdgeInsets.only(left: 30, right: 30, top: 14),
       child: Row(
         children: [
           Expanded(
@@ -40,7 +56,6 @@ class _CustomDropDownItemState extends State<CustomDropDownItem> {
                 ),
                 filled: true,
                 fillColor: Colors.white,
-              
               ),
               value: selectedYear,
               items: widget.listValues.map((value) {
@@ -49,15 +64,23 @@ class _CustomDropDownItemState extends State<CustomDropDownItem> {
                   child: Text(value.toString()),
                 );
               }).toList(),
-              onChanged: (String? value) {
-                setState(() {
-                  selectedYear = value;
-                  widget.controller.text = value?.toString() ?? '';
-                });
-              },
+              onChanged: widget.isEnabled
+                  ? (String? value) {
+                      setState(() {
+                        selectedYear = value;
+                        widget.controller.text = value?.toString() ?? '';
+                      });
+                      if (widget.onChanged != null) {
+                        widget.onChanged!(value ?? '');
+                      }
+                    }
+                  : null, // Set onChanged to null if disabled
+              autovalidateMode: widget.isEnabled
+                  ? AutovalidateMode.always
+                  : AutovalidateMode
+                      .disabled, // Set autovalidateMode based on isEnabled
             ),
           ),
-         
         ],
       ),
     );
